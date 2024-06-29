278f0890a29456797cbf9a68fb4b234c075fcaf7
styv <styv2301@gmail.com>
2024-06-28 23:18:45 -0500

    inject content after deactivating or reloading the extension

--- background.js
+++ background.js
@@ -1,47 +1,16 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.tabs.query({}, (tabs) => {
-//         for (let tab of tabs) {
-
-//             chrome.scripting.executeScript({
-//                 target: { tabId: tab.id },
-//                 files: ['content.js']
-//             }, () => {
-//                 console.log('Content script inyectado en la pestaña', tab.id);
-//             });
-
-//         }
-//     });
-// })
-
-// chrome.scripting
-//     .registerContentScripts([{
-//         id: "session-script",
-//         js: ["content.js"],
-//         persistAcrossSessions: false,
-//         matches: ["<all_urls>"],
-//         runAt: "document_idle",
-//         allFrames: true,
-//     }])
-//     .then(() => console.log("registration complete"))
-//     .catch((err) => console.warn("unexpected error", err))
-
-
-// chrome.tabs.query({}, (tabs) => {
-
-//     for (let tab of tabs) {
-//         if (tab.url.match(/(chrome|chrome-extension):\/\//gi)) {
-//             continue;
-//         }
-//         chrome.scripting.executeScript({
-//             target: { tabId: tab.id },
-//             files: ["content.js"],
-//         }).then(() => console.log("script injected in all jaus"));
-//     }
-// })
-
-// chrome.tabs.query({ url: ['https://ahiseve.vercel.app/*', 'http://localhost:4321/*'] }, (tabs) => {
-//     console.log(tabs)
-// })
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('installed')
+})
+chrome.tabs.query({ url: ['https://ahiseve.vercel.app/*', 'http://localhost:4321/*'] }, (tabs) => {
+    tabs.forEach(tab => {
+        chrome.scripting.executeScript({
+            target: { tabId: parseInt(tab.id) },
+            files: ["content.js"],
+        }).then(() => {
+            console.log("script injected")
+        });
+    })
+})
 
 let dataG = {}
 
@@ -68,10 +37,10 @@ function comprobarData() {
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         comprobarData().then(() => {
-            console.log(request);
+            // console.log(request);
             // console.log(sender);
             if (request.cmd == 'updateDataG') {
-                console.log('si se pudo chavito')
+                // console.log('si se pudo chavito')
                 dataG = request.data
 
                 chrome.storage.local.set({ dataG: JSON.stringify(request.data) }, function () {
@@ -80,7 +49,7 @@ chrome.runtime.onMessage.addListener(
 
             }
             else if (request.cmd == 'element-action') {
-                console.log('element-action', request)
+                // console.log('element-action', request)
                 if (request.data.status == 'sending') {
                     // enviar al pagina principal
                     chrome.tabs.query({ url: ['https://ahiseve.vercel.app/*', 'http://localhost:4321/*'] }, (tabs) => {
@@ -92,7 +61,7 @@ chrome.runtime.onMessage.addListener(
                         });
                     })
                 } else {
-                    console.log(dataG)
+                    // console.log(dataG)
                     // enviar a la pagina y videoElement Selecionado
                     chrome.tabs.sendMessage(parseInt(dataG.tabId), {
                         cmd: request.cmd,
@@ -114,9 +83,9 @@ chrome.runtime.onMessage.addListener(
                     sendData = {
                         selected: true,
                     }
-                    console.log('dataG', dataG)
+                    // console.log('dataG', dataG)
                 }
-                console.log('dataG vacio', dataG)
+                // console.log('dataG vacio', dataG)
                 chrome.tabs.sendMessage(sender.tab.id, {
                     cmd: 'resultCheckElementVideoSelected',
                     data: sendData
