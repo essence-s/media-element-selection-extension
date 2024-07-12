011a9790bcc6602e083b4b1f264d23d1d310799f
styv <styv2301@gmail.com>
2024-07-11 22:30:26 -0500

    return an error message in the reject

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@ export function sendMessage(message) {
     return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage(message, (response) => {
             if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError)
+                reject(chrome.runtime.lastError.message)
             } else {
                 resolve(response)
             }
